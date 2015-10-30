0dafa61d6cc3edb981f2aacd3fffeb4b9dc71253
Phan Van Thanh <thanhpv@fimo.edu.vn>
2015-10-30 14:02:41 +0700

    HttpBasicAuth custom

---
+++ fproject/authclient/HttpBasicAuth.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\components;
+
+use fproject\authclient\OAuth2;
+use Yii;
+
+class HttpBasicAuth extends \yii\filters\auth\HttpBasicAuth
+{
+    /**
+     * @inheritdoc
+     */
+    public function authenticate($user, $request, $response)
+    {
+        $authHeader = $request->getHeaders()->get('Authorization');
+        if ($authHeader !== null && preg_match("/^Basic\\s+(.*?)$/", $authHeader, $matches)) {
+            /** @var OAuth2|null $authClient */
+            $authClient = OAuth2::getInstance();
+            if($authClient)
+            {
+                /** @var String $authString */
+                $authString = base64_encode($authClient->clientId . ":" . $authClient->clientSecret);
+                if(strcmp($matches[1], $authString) == 0) {
+                    return true;
+                } else {
+                    $this->handleFailure($response);
+                }
+            }
+        }
+        return null;
+    }
+}
