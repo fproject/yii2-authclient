5819e8fd5162ed15a8a8ede9ebd5f3a5ebb5c36a
Phan Van Thanh <thanhpv@fimo.edu.vn>
2015-11-23 18:22:54 +0700

    Fix #1

--- fproject/authclient/AuthRevokeActionFilter.php
+++ fproject/authclient/AuthRevokeActionFilter.php
@@ -16,8 +16,11 @@ class AuthRevokeActionFilter extends ActionFilter
         if( !Yii::$app->user->getIsGuest() && $authClient)
         {
             /** @var OAuthToken $at */
-            $at = $authClient->getAccessToken()->token;
-            $rawPayload = OAuth2::getInstance()->verifyAndDecodeToken($at, true);
+            $at = $authClient->getAccessToken();
+            if(!empty($at))
+            {
+                $rawPayload = OAuth2::getInstance()->verifyAndDecodeToken($at->token, true);
+            }
             if(empty($rawPayload) || !property_exists($rawPayload, 'sub')) {
                 return null;
             }
